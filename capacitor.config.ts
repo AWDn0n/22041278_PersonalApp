c4a36e39af81d977cfd06e4726a709fb900ed03e
Pablo Ribas Borrego <pablo.ribas.borrego@gmail.com>
2024-09-26 09:21:41 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '22041278_PersonalApp',
+  webDir: 'www'
+};
+
+export default config;
